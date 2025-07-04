ea7c827eb76642e45949f788af22e0130068edca
axelutn <axeldzumagautn@gmail.com>
2025-07-03 22:36:45 -0300

    avance del tp

---
+++ provision_fedora.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Instalando paquetes básicos en Fedora..."
+sudo dnf install -y htop tmux curl
---
+++ provision_ubuntu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Instalando paquetes básicos..."
+sudo apt update -y && sudo apt install -y htop tmux curl
